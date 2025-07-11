df5781c3e484ba1b5910dab2bb74a24a07122dcc
Austin Eziulo <aeziulo@gmail.com>
2025-07-11 19:18:31 +0100

    11th July 2025

---
+++ script.js
@@ -0,0 +1,43 @@
+var internName = "Eziulo Austin";
+let taskOne = 8;
+let taskTwo = 8;
+let taskThree = 9;
+const totalScore = (taskOne + taskTwo + taskThree);
+const averageScore = (totalScore / 3);
+
+const average = averageScore;
+let grade;
+if (average>=9) {
+    grade = "A";
+} else if (average>=8) {
+    grade = "B";
+} else if (average>=7) {
+    grade = "C";
+} else if (average>=6) {
+    grade = "D";
+} else if (average<6) {
+    grade = "E";
+}
+
+let feedback;
+switch (grade) {
+  case "A":
+    feedback="Excellent performance!";
+    break;
+  case "B":
+    feedback="Great job! Keep aiming high.";
+    break;
+  case "C":
+    feedback="Good effort. Try to improve.";
+    break;
+  case "D":
+    feedback="You're close. Keep working at it.";
+    break;
+  case "E":
+    feedback="Don't give up. You can do better next time.";
+    break;
+  default:
+    feedback="Invalid grade";
+}
+
+console.log(`Student Name: ${internName} \nTask One Score: ${taskOne} \nTask Two Score: ${taskTwo} \nTask Three Score: ${taskThree} \nAverage: ${average.toFixed(2)} \nFinal Grade: ${grade} \nMessage: ${feedback}`)
